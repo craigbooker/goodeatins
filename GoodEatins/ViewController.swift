b527a4419080554724b7eff20cf144991912f32c
Craig Booker <craig@arcadiasoftworks.com>
2020-02-07 13:33:52 -0600

    Initial Commit

---
+++ GoodEatins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins
+//
+//  Created by Craig Booker on 2/7/20.
+//  Copyright © 2020 Arcadia Softworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
